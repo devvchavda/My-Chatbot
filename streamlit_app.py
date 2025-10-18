8dc6f2b5703d9b052ee5bb9a45a184ec46f7a00a
devvchavda <devchavda732@gmail.com>
2025-10-18 15:58:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,7 +123,7 @@ if "current_chat_id" in st.session_state:
                     stream_mode="messages",
                 ):
                     if isinstance(message, AIMessage):
-                        full_response += message.content or ""
+                        full_response += message.content
                     elif isinstance(message, ToolMessage):
                         st.info("Using Appropriate tool")
                     response_placeholder.markdown(full_response + " ")
